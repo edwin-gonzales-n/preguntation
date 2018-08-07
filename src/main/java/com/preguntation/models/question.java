a98d6be87e5f4c6ae4ed49dde0c03a2d91161acd
edwin-gonzales-n <edwin.gonzales.n@gmail.com>
2018-08-07 14:53:59 -0500

    adding question model

--- src/main/java/com/preguntation/models/question.java
+++ src/main/java/com/preguntation/models/question.java
@@ -1,4 +1,37 @@
 package com.preguntation.models;
 
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="questions")
 public class question {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @NotBlank(message = "Must have question")
+    @Column(nullable = false)
+    private String question;
+
+    @NotBlank(message = "Must have correct answer")
+    @Column(nullable = false)
+    private long correct_answer;
+
+    public question(){}
+
+    //insert into db
+    public question(String question, long correct_answer) {
+        this.question = question;
+        this.correct_answer = correct_answer;
+    }
+
+    //pull from db
+    public question(long id,String question, long correct_answer) {
+        this.id = id;
+        this.question = question;
+        this.correct_answer = correct_answer;
+    }
 }
