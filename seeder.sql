0becf754dab123c7a4df6fe2491291b5e800ce1b
edwin-gonzales-n <edwin.gonzales.n@gmail.com>
2018-08-13 11:24:54 -0500

    adding seeder for questions and answers.  Small sample.

---
+++ seeder.sql
@@ -0,0 +1,37 @@
+use preguntation_db;
+
+INSERT INTO questions (question, correct_answer) VALUES
+('En que año estamos?', 3),
+('Nombre del presidente del gobierno de España', 10),
+('Ganador de la liga de España 2013', 15),
+('Ganador mundial f1 2012', 11),
+('Como se llama el muñeco naranja de Barrio Sesamo', 17),
+('Como se llama la pasta cuadrada rellena', 20),
+('Cuántas películas tiene la saga de harry potter', 25),
+('En que año salio el disco de Manolo García pájaros de barro', 27);
+
+INSERT INTO answers (id_question, answer) VALUES
+(1, '2011'),
+(1, '2012'),
+(1, '2013'),
+(2, 'Zapatero'),
+(2, 'Aznar'),
+(2, 'Rajoy'),
+(3, 'Atletico de Madrid'),
+(3, 'Barcelona'),
+(3, 'Madrid'),
+(4, 'Vetel'),
+(4, 'Alonso'),
+(4, 'Hamilton'),
+(5, 'Epi'),
+(5, 'Blas'),
+(5, 'La gallina caponata'),
+(6, 'ravioli'),
+(6, 'tortellini'),
+(6, 'macarron'),
+(7, '3'),
+(7, '6'),
+(7, '8'),
+(8, '1995'),
+(8, '1998'),
+(8, '2000');
