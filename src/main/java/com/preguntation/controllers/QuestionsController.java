5608ffbfcedb6080d2bc6c1e53bf3b3195211b00
edwin-gonzales-n <edwin.gonzales.n@gmail.com>
2018-08-15 00:05:04 -0500

    adding logic for getting the submitted answer from user. Must look into only submitting the answer picked by the user using Javascript.

--- src/main/java/com/preguntation/controllers/QuestionsController.java
+++ src/main/java/com/preguntation/controllers/QuestionsController.java
@@ -1,5 +1,6 @@
 package com.preguntation.controllers;
 
+import com.preguntation.models.answer;
 import com.preguntation.models.question;
 import com.preguntation.repositories.AnswersRepository;
 import com.preguntation.repositories.QuestionsRepository;
@@ -8,7 +9,11 @@ import com.preguntation.repositories.UsersRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class QuestionsController {
@@ -29,9 +34,26 @@ public class QuestionsController {
         long randomId = (long)(Math.random()*(8+1));
         question questions = questionsRepository.findOne(randomId);
         model.addAttribute("questions", questions);
-
+        List<answer> answers = answersRepository.findAllByQuestion_ID(randomId);
+        model.addAttribute("answers", answers);
         return "game/trivia";
     }
 
+    @PostMapping("/triviaSubmit")
+    public String getAnswer(Model model,
+                            @RequestParam(value = "id") String answer_id,
+                            @RequestParam(value = "correct_answer_id") String correct_answer_id) {
+        System.out.println("The user's answer is: " + answer_id);
+        System.out.println("The correct answer is: " + correct_answer_id);
 
+        if(answer_id.equals(correct_answer_id)){
+            String message = "Yes, that is the correct answer!";
+            model.addAttribute("correct_answer", message);
+            return "redirect:/trivia";
+        } else{
+            String wrong = "Nope!";
+            model.addAttribute("wrong_answer", wrong);
+            return "redirect:/trivia";
+        }
+    }
 }
--- src/main/java/com/preguntation/repositories/AnswersRepository.java
+++ src/main/java/com/preguntation/repositories/AnswersRepository.java
@@ -1,10 +1,16 @@
 package com.preguntation.repositories;
 
 import com.preguntation.models.answer;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AnswersRepository extends CrudRepository<answer, Long> {
-
+//    Iterable<answer> findAllById_question(long id_question);
+//    Iterable<answer> findAllById_question(long n);
+    @Query(nativeQuery =true, value = "SELECT * from answers WHERE id_question = ?1")
+    List<answer> findAllByQuestion_ID(long n);
 }
