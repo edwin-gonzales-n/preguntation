415cbfe4f88c429da2b6b9350421d784687f1cf2
edwin-gonzales-n <edwin.gonzales.n@gmail.com>
2018-08-23 00:32:16 -0500

    added json functionality to questions controller.  Now i can get all questions with an ajax request. Must work on UI and also on the correct iteration for all the questions.

--- src/main/java/com/preguntation/controllers/QuestionsController.java
+++ src/main/java/com/preguntation/controllers/QuestionsController.java
@@ -6,14 +6,20 @@ import com.preguntation.repositories.AnswersRepository;
 import com.preguntation.repositories.QuestionsRepository;
 import com.preguntation.repositories.RankingRepository;
 import com.preguntation.repositories.UsersRepository;
+import com.preguntation.services.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-@Controller
+@RestController
 public class QuestionsController {
+
+    @Autowired
+    public QuestionService questionService;
+
     public AnswersRepository answersRepository;
     public QuestionsRepository questionsRepository;
     public UsersRepository usersRepository;
@@ -26,36 +32,49 @@ public class QuestionsController {
         this.rankingRepository = rankingRepository;
     }
 
-    @GetMapping("/trivia")
-    public String getQuestion(Model model) {
-
-        long count = questionsRepository.count();
-        System.out.println("The table count is: " + count);
-
-        //  creating array list to store each number id of the total count of the questions table.
-        List numbers = new ArrayList();
-        long i = 1;
-        while(i <= count){
-            System.out.println("value of i is: " + i);
-            numbers.add(i);
-            i++;
-        }
-
-        //created this logic to attempt to send to frontend just one question in order.
-        List<question> roles = new ArrayList<>();
-        questionsRepository.findAll().forEach(roles::add);
-        System.out.println("This is the output of roles" + roles);
-        //
-
-        long random = new Random().nextInt(((int)count - 1))+1;
-        System.out.println("randomId = " + random);
-        question questions = questionsRepository.findOne(random);
-        model.addAttribute("questions", questions);
-        List<answer> answers = answersRepository.findAllByQuestion_ID(random);
-        model.addAttribute("answers", answers);
-
-        return "game/trivia";
+    // using the serviceRepo instead.
+    @GetMapping("/triviaAllQuestions")
+    public Iterable<question> getQuestions(){
+        return questionService.findAll();
     }
+
+    @GetMapping("/triviaEachQuestion")
+    public question getQuestion(){
+        long count = questionsRepository.count();
+        long random = new Random().nextInt(((int)count - 1))+1;
+        return questionService.findQuestion(random);
+    }
+
+//    @GetMapping("/trivia")
+//    public String getQuestion(Model model) {
+//
+//        long count = questionsRepository.count();
+//        System.out.println("The table count is: " + count);
+//
+//        //  creating array list to store each number id of the total count of the questions table.
+//        List numbers = new ArrayList();
+//        long i = 1;
+//        while(i <= count){
+//            System.out.println("value of i is: " + i);
+//            numbers.add(i);
+//            i++;
+//        }
+//
+//        //created this logic to attempt to send to frontend just one question in order.
+//        List<question> roles = new ArrayList<>();
+//        questionsRepository.findAll().forEach(roles::add);
+//        System.out.println("This is the output of roles" + roles);
+//        //
+//
+//        long random = new Random().nextInt(((int)count - 1))+1;
+//        System.out.println("randomId = " + random);
+//        question questions = questionsRepository.findOne(random);
+//        model.addAttribute("questions", questions);
+//        List<answer> answers = answersRepository.findAllByQuestion_ID(random);
+//        model.addAttribute("answers", answers);
+//
+//        return "game/trivia";
+//    }
 
     @PostMapping("/triviaSubmit")
     public String getAnswer(Model model,
